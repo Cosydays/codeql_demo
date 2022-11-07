5bb53a9b3aba04085f6542d8599b40755803f070
caijiahui.jiahui <caijiahui.jiahui@bytedance.com>
2022-11-07 11:51:07 +0800

    Add mysql client

--- client/mysql.go
+++ client/mysql.go
@@ -1,1 +1,19 @@
 package client
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	MysqlDb *gorm.DB
+)
+
+func InitMysql() {
+	dsn := "root:root123@tcp(127.0.0.1:3306)/test_gorm?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	MysqlDb, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
